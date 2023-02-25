adbc3d2ec8798e79f23116e13cb3fcee2fa6c180
klaudiabilska <klaudiabilska1@gmail.com>
2023-02-25 12:20:47 +0100

    js

---
+++ app.js
@@ -0,0 +1,15 @@
+const front = document.querySelector(".content-one")
+const back = document.querySelector(".content-two")
+const cardClick = document.querySelector(".card-front")
+const cardClickBack = document.querySelector(".card-back")
+
+
+cardClick.addEventListener("click", () => {
+    back.classList.remove("hidden")
+    front.classList.add("hidden")
+})
+
+cardClickBack.addEventListener("click", () => {
+    front.classList.remove("hidden")
+    back.classList.add("hidden")
+})
